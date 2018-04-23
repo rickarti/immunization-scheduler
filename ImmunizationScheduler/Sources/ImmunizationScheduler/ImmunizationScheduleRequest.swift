5f0ef080b7a87af592db167fdcfb5776ce436dcb
Richard Johnson <rickarti@sbcglobal.net>
2018-04-22 20:01:08 -0700

    Added an ImmunizationScheduleRequest struct

---
+++ ImmunizationScheduler/Sources/ImmunizationScheduler/ImmunizationScheduleRequest.swift
@@ -0,0 +1,12 @@
+//
+//  ImmunizationScheduleRequest.swift
+//  ImmunizationScheduler
+//
+//  Created by Richard Johnson on 4/22/18.
+//
+
+import Foundation
+
+struct ImmunizationScheduleRequest {
+    
+}
--- ImmunizationScheduler/Tests/ImmunizationSchedulerTests/ImmunizationSchedulerTests.swift
+++ ImmunizationScheduler/Tests/ImmunizationSchedulerTests/ImmunizationSchedulerTests.swift
@@ -2,15 +2,22 @@ import XCTest
 @testable import ImmunizationScheduler
 
 final class ImmunizationSchedulerTests: XCTestCase {
+    
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
         XCTAssertEqual(ImmunizationScheduler().text, "Hello, World!")
     }
+    
+    func testCreateRequest() {
+        let request: ImmunizationScheduleRequest! = ImmunizationScheduleRequest()
+        XCTAssertNotNil(request)
+    }
 
 
     static var allTests = [
         ("testExample", testExample),
+        ("testCreateRequest", testCreateRequest),
     ]
 }
