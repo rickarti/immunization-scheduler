5454332558c96c58c6d85bfba8532dea9e16cfc8
Rick Johnson <rpjohnson@nextgen.com>
2018-04-24 07:07:17 -0700

    adding DoseType

---
+++ ImmunizationScheduler/Sources/ImmunizationScheduler/DoseType.swift
@@ -0,0 +1,19 @@
+//
+//  DoseType.swift
+//  ImmunizationScheduler
+//
+//  Created by Rick Johnson on 4/24/18.
+//
+
+import Foundation
+
+enum DoseType {
+    case firstDose
+    case secondDose
+    case thirdDose
+    case fourthDose
+    case fifthDose
+    case booster
+    case annualVaccination
+    case invlidEarly
+}
