d9b0b1cbfb85ffdfa68920685b205623f5ef0ff3
Rick Johnson <rpjohnson@nextgen.com>
2018-04-24 08:35:45 -0700

    adding VaccineType

--- ImmunizationScheduler/Sources/ImmunizationScheduler/VaccineType.swift
+++ ImmunizationScheduler/Sources/ImmunizationScheduler/VaccineType.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+
+enum VaccineType {
+    case HEP_B // ("45")
+    case RV // ("122")
+    case DTAP // ("107")
+    case HI_B // ("17")
+    case PCV_13 // ("152")
+    case IPV // ("89")
+    case INFLUENZA // ("88")
+    case MMR // ("03")
+    case VAR // ("21")
+    case HEP_A // ("85")
+    case MENINGOCOCCAL // ("108")
+    case TDAP // ("115")
+    case TD // ("139")
+    case HPV // ("137")
+    case HZV // ("121")
+    case PPSV23 // ("33")
+    case MENINGOCOCCAL_B // ("")
+}
