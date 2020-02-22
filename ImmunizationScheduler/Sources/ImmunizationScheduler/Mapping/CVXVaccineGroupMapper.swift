322d616f116b1aa54beca34bf8f37d95e899d43f
Richard Johnson <rickarti@sbcglobal.net>
2020-02-22 11:30:03 -0800

    Adding CVXVaccineGroupMapper and Test class

---
+++ ImmunizationScheduler/Sources/ImmunizationScheduler/Mapping/CVXVaccineGroupMapper.swift
@@ -0,0 +1,15 @@
+//
+//  CVXVaccineGroupMapper.swift
+//  ImmunizationScheduler
+//
+//  Created by Richard Johnson on 2/22/20.
+//
+
+import Foundation
+
+struct CVXVaccineGroupMapper {
+    
+    static func getMappings() {
+        
+    }
+}
---
+++ ImmunizationScheduler/Tests/ImmunizationSchedulerTests/Mapping/CVXVaccineGroupMapperTests.swift
@@ -0,0 +1,18 @@
+//
+//  CVXVaccineGroupMapperTests.swift
+//  ImmunizationSchedulerTests
+//
+//  Created by Richard Johnson on 2/22/20.
+//
+
+import XCTest
+@testable import ImmunizationScheduler
+
+class CVXVaccineGroupMapperTests: XCTestCase {
+
+    func testGetMappings_returnsValidMappings() {
+        CVXVaccineGroupMapper.getMappings()
+        
+        
+    }
+}
