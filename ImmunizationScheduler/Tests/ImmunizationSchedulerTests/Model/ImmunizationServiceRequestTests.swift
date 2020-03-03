3d84aaaba4d016a706ad09b9480ba2f1802c4363
Richard Johnson <rickarti@sbcglobal.net>
2020-03-03 14:27:28 -0800

    Adding some tests

--- ImmunizationScheduler/Tests/ImmunizationSchedulerTests/Model/ImmunizationServiceRequestTests.swift
+++ ImmunizationScheduler/Tests/ImmunizationSchedulerTests/Model/ImmunizationServiceRequestTests.swift
@@ -6,28 +6,19 @@
 //
 
 import XCTest
+@testable import ImmunizationScheduler
 
 class ImmunizationServiceRequestTests: XCTestCase {
 
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+
+    func test_init_datesAreCorrect() {
+        let request = ImmunizationScheduleRequest(requestDate: Date(), birthDate: Date(), givenDoses: [GivenDose]())
+
+        print(request.requestDate)
+        print(request.birthDate)
+        
     }
 
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
     
 //    Wed Jun 14 00:00:00 PDT 2017
 //    Sat Oct 14 00:00:00 PDT 2017
--- ImmunizationScheduler/Tests/ImmunizationSchedulerTests/Schedulers/HepADoseSchedulerTests.swift
+++ ImmunizationScheduler/Tests/ImmunizationSchedulerTests/Schedulers/HepADoseSchedulerTests.swift
@@ -1,32 +1,58 @@
-//
-//  HepADoseSchedulerTests.swift
-//  ImmunizationSchedulerTests
-//
-//  Created by Richard Johnson on 2/29/20.
-//
-
 import XCTest
+@testable import ImmunizationScheduler
 
 class HepADoseSchedulerTests: XCTestCase {
 
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+
+    func test_noDosesGiven_returnTwoDueDoses() {
+        
+        // let response = ImmunizationScheduleResponse()
+        // let scheduler = HepADoseScheduler()
+        
+
+//
+//        let request: ImmunizationScheduleRequest = ImmunizationScheduleRequest(requestDate: Date(), birthDate: Date(), givenDoses: [GivenDose]())
+//
+//        XCTAssertEqual(Date(), request.birthDate)
+        
+        //let immunizationSchedule = scheduler.immunizationSchedule(request: request, response: response)[0]
+        
+        //immunizationSchedule.
+        // let scheduledDoses = immunizationSchedule[0]
+        
+        
+        
+        // XCTAssertEqual(.HEP_A, response.immunizationSchedules[0].vaccineType)
+        // XCTAssertEqual(2, scheduledDoses.count)
+        
+        
+//        assertThat(hepADoses.size(), is(2));
+//        assertDose(hepADoses.get(0), DoseType.FIRST_DOSE, "1/1/2017", "1/1/2018", "1/1/2017", null, VaccineType.HEP_A, false);
+//        assertDose(hepADoses.get(1), DoseType.SECOND_DOSE, "7/1/2017", "7/1/2018", "7/1/2017", null, VaccineType.HEP_A, false);
+        
+        
+        let cal = Calendar(identifier: .gregorian)
+        let dateComponents = DateComponents(calendar: cal, timeZone: .current  , year: 2020, month: 2, day: 29)
+        
+        
+        
+        let birthDate: Date = dateComponents.date!
+        
+        print("Birth Date: \(birthDate)")
+        
+        print("Birth Date description: \(birthDate.description)")
+        
+        
+        let dateFormat = DateFormatter()
+        dateFormat.locale = Locale(identifier: "en_US_POSIX")
+        dateFormat.dateFormat = "M/d/y"
+        let requestDate = dateFormat.date(from: "11/15/2020")!
+        
+        print("Request Date: \(requestDate)")
+        
+        print("Request Date description: \(requestDate.description)")
     }
 
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
 
 }
+
